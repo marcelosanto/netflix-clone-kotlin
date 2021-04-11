c16f663e8c314ae876cf96ba776e790c108fe411
Marcelo Santos <rocha.mer21@gmail.com>
2021-04-11 16:20:18 -0300

    feat: Tela  de cadastro terminada

--- app/src/main/java/com/marcelo/netflixclone/FormCadastro.kt
+++ app/src/main/java/com/marcelo/netflixclone/FormCadastro.kt
@@ -2,10 +2,19 @@ package com.marcelo.netflixclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.marcelo.netflixclone.databinding.ActivityFormCadastroBinding
 
 class FormCadastro : AppCompatActivity() {
+    private lateinit var binding: ActivityFormCadastroBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_form_cadastro)
+        binding = ActivityFormCadastroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar!!.hide()
+
+        val toolbar = binding.toolbarCadastro
+        toolbar.setBackgroundColor(getColor(R.color.white))
+        toolbar.setNavigationIcon(getDrawable(R.drawable.ic_netflix_official_logo))
     }
 }
