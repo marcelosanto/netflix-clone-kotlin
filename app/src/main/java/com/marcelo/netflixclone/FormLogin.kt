07a387057264d04be704909198559af02011d7b1
Marcelo Santos <rocha.mer21@gmail.com>
2021-04-11 13:13:10 -0300

    feat: Tela  de login terminada, iniciando a tela de cadastro

--- app/src/main/java/com/marcelo/netflixclone/FormLogin.kt
+++ app/src/main/java/com/marcelo/netflixclone/FormLogin.kt
@@ -1,13 +1,23 @@
 package com.marcelo.netflixclone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.marcelo.netflixclone.databinding.ActivityFormLoginBinding
 
 class FormLogin : AppCompatActivity() {
+    private lateinit var binding: ActivityFormLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_form_login)
+        binding = ActivityFormLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         supportActionBar!!.hide()
+
+        binding.txtTelaCadastro.setOnClickListener {
+            val intent = Intent(this, FormCadastro::class.java)
+            startActivity(intent)
+        }
     }
 }
