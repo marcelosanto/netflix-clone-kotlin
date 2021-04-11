d9d7595498872ecc62557ee26db3f7d560c41c3b
Marcelo Santos <rocha.mer21@gmail.com>
2021-04-11 00:26:17 -0300

    feat: Splash Screen Criado e Transição para tela  de login

--- app/src/main/java/com/marcelo/netflixclone/MainActivity.kt
+++ app/src/main/java/com/marcelo/netflixclone/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.marcelo.netflixclone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar!!.hide()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            AbrirTeladeLogin()
+        }, 2000)
+    }
+
+    private fun AbrirTeladeLogin(){
+        val intent = Intent(this, FormLogin::class.java)
+        startActivity(intent)
+        finish()
     }
 }
