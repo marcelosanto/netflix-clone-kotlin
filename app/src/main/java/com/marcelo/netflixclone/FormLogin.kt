05ac3538d7266e6d1f7d337b31b06f15bed025cd
Marcelo Santos <rocha.mer21@gmail.com>
2021-04-12 19:47:33 -0300

    feat: Funcao AutenticarUsuario() criada

--- app/src/main/java/com/marcelo/netflixclone/FormLogin.kt
+++ app/src/main/java/com/marcelo/netflixclone/FormLogin.kt
@@ -3,6 +3,8 @@ package com.marcelo.netflixclone
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import com.marcelo.netflixclone.databinding.ActivityFormLoginBinding
 
 class FormLogin : AppCompatActivity() {
@@ -19,5 +21,40 @@ class FormLogin : AppCompatActivity() {
             val intent = Intent(this, FormCadastro::class.java)
             startActivity(intent)
         }
+
+        binding.btEntrar.setOnClickListener {
+            val email = binding.editLoginEmail.text.toString()
+            val senha = binding.editLoginPassword.text.toString()
+            val mensagem = binding.mensagemErro
+
+            if(email.isEmpty() || senha.isEmpty()) {
+                mensagem.setText("Preencha os campos")
+            }
+            else {
+                AutenticarUsuario()
+            }
+        }
+    }
+
+    private  fun AutenticarUsuario(){
+        val email = binding.editLoginEmail.text.toString()
+        val senha = binding.editLoginPassword.text.toString()
+        val mensagem = binding.mensagemErro
+
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, senha).addOnCompleteListener {
+            if(it.isSuccessful) {
+                Toast.makeText(this, "Login efetuado com Sucesso!", Toast.LENGTH_SHORT).show()
+                IrParaTelaDeFilmes()
+            }
+        }.addOnFailureListener {
+            mensagem.setText("Erro ao logar usuário")
+        }
+
+    }
+
+    private fun IrParaTelaDeFilmes(){
+        val intent = Intent(this, ListaFilmes::class.java)
+        startActivity(intent)
+        finish()
     }
 }
