450c81df13f8fe3acf6cd9d8190083799a01b550
Marcelo Santos <rocha.mer21@gmail.com>
2021-04-12 18:17:28 -0300

    feat: Função CadastrarUsuario() criada

--- app/src/main/java/com/marcelo/netflixclone/FormCadastro.kt
+++ app/src/main/java/com/marcelo/netflixclone/FormCadastro.kt
@@ -2,6 +2,8 @@ package com.marcelo.netflixclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import com.marcelo.netflixclone.databinding.ActivityFormCadastroBinding
 
 class FormCadastro : AppCompatActivity() {
@@ -12,11 +14,42 @@ class FormCadastro : AppCompatActivity() {
         setContentView(binding.root)
 
         supportActionBar!!.hide()
+        Toolbar()
 
+        binding.btCadastrar.setOnClickListener {
+            var email = binding.editCadastroEmail.text.toString()
+            var senha = binding.editCadastroSenha.text.toString()
+            var mensagem = binding.mensagemErroCadastro
+
+            if(email.isEmpty() || senha.isEmpty()){
+                mensagem.setText("Preencha todos os campos")
+            } else {
+                CadastrarUsuario()
+            }
+        }
+
+    }
+
+    private  fun CadastrarUsuario(){
+        var email = binding.editCadastroEmail.text.toString()
+        var senha = binding.editCadastroSenha.text.toString()
+        var mensagem = binding.mensagemErroCadastro
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, senha).addOnCompleteListener {
+            if(it.isSuccessful){
+                Toast.makeText(this, "Usuário cadastrado com sucesso!", Toast.LENGTH_SHORT).show()
+                binding.editCadastroEmail.setText("")
+                binding.editCadastroSenha.setText("")
+                binding.mensagemErroCadastro.setText("")
+            }
+        }.addOnFailureListener {
+            mensagem.setText("Erro ao cadastrar usuário.")
+        }
+    }
+
+    private  fun Toolbar(){
         val toolbar = binding.toolbarCadastro
         toolbar.setBackgroundColor(getColor(R.color.white))
         toolbar.setNavigationIcon(getDrawable(R.drawable.ic_netflix_official_logo))
-
-
     }
 }
