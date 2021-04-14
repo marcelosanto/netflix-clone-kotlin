b831ea94ed82670e1c0e74e1e740e2fd08518763
Marcelo Santos <rocha.mer21@gmail.com>
2021-04-14 20:09:07 -0300

    feat: Função deslogar criada

--- app/src/main/java/com/marcelo/netflixclone/ListaFilmes.kt
+++ app/src/main/java/com/marcelo/netflixclone/ListaFilmes.kt
@@ -1,11 +1,45 @@
 package com.marcelo.netflixclone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.google.firebase.auth.FirebaseAuth
+import com.marcelo.netflixclone.databinding.ActivityListaFilmesBinding
+import java.util.zip.Inflater
 
 class ListaFilmes : AppCompatActivity() {
+    private lateinit var binding: ActivityListaFilmesBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_lista_filmes)
+        binding = ActivityListaFilmesBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflate = menuInflater
+        inflate.inflate(R.menu.menu_principal, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.deslogar -> {
+                FirebaseAuth.getInstance().signOut()
+                VoltarTelaDeLogin()
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun VoltarTelaDeLogin(){
+        val intent = Intent(this, FormLogin::class.java)
+        startActivity(intent)
+        finish()
     }
 }
