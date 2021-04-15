e4fbde57d9afe36a760fe0aa6e02608861040861
Marcelo Santos <rocha.mer21@gmail.com>
2021-04-15 11:16:28 -0300

    feat: criado o scroll na tela, botao voltar na toolbar

--- app/src/main/java/com/marcelo/netflixclone/DetalhesFilme.kt
+++ app/src/main/java/com/marcelo/netflixclone/DetalhesFilme.kt
@@ -1,11 +1,37 @@
 package com.marcelo.netflixclone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import com.marcelo.netflixclone.Adapter.FilmesAdapter
+import com.marcelo.netflixclone.Model.addFilmes
+import com.marcelo.netflixclone.databinding.ActivityDetalhesFilmeBinding
 
 class DetalhesFilme : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetalhesFilmeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detalhes_filme)
+        binding = ActivityDetalhesFilmeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar!!.hide()
+        Toolbar()
+
+        val recycler_outros_filmes = binding.recyclerOutrosFilmes
+        recycler_outros_filmes.adapter = FilmesAdapter(addFilmes())
+        recycler_outros_filmes.layoutManager = GridLayoutManager(applicationContext, 3)
+    }
+
+    private  fun Toolbar() {
+        val toolbar_detalhes = binding.toolbarDetalhes
+        toolbar_detalhes.setNavigationIcon(getDrawable(R.drawable.ic_voltar))
+        toolbar_detalhes.setNavigationOnClickListener {
+            val intent = Intent(this, ListaFilmes::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
