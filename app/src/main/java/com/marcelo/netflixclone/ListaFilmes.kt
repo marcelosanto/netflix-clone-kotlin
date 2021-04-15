fb867d1c07adf4568bd16c60f31601c53d07cfcf
Marcelo Santos <rocha.mer21@gmail.com>
2021-04-15 09:13:42 -0300

    feat: criado a click dos filmes e enviando id pra tela de detalhe

--- app/src/main/java/com/marcelo/netflixclone/ListaFilmes.kt
+++ app/src/main/java/com/marcelo/netflixclone/ListaFilmes.kt
@@ -5,11 +5,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import android.widget.GridLayout
 import androidx.recyclerview.widget.GridLayoutManager
 import com.google.firebase.auth.FirebaseAuth
 import com.marcelo.netflixclone.Adapter.FilmesAdapter
 import com.marcelo.netflixclone.Model.addFilmes
+import com.marcelo.netflixclone.OnClick.OnItemClickListener
+import com.marcelo.netflixclone.OnClick.addOnItemClickListener
 import com.marcelo.netflixclone.databinding.ActivityListaFilmesBinding
 import java.util.zip.Inflater
 
@@ -25,6 +28,18 @@ class ListaFilmes : AppCompatActivity() {
         recycler_filmes.adapter = FilmesAdapter(addFilmes())
         recycler_filmes.layoutManager = GridLayoutManager(applicationContext, 3)
 
+        recycler_filmes.addOnItemClickListener(object: OnItemClickListener{
+            override fun onItemClicked(position: Int, view: View) {
+                when{
+                    position == 0 -> DetalhesFilme()
+                }
+            }
+        })
+    }
+
+    private  fun DetalhesFilme(){
+        val intent = Intent(this, DetalhesFilme::class.java)
+        startActivity(intent)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
