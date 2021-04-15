4a0e26885bd383e4d7f20072c93395fa776a23c0
Marcelo Santos <rocha.mer21@gmail.com>
2021-04-15 12:44:26 -0300

    feat: add a biblicoteca Picasso, travado o tipo de visualizacao do app

--- app/src/main/java/com/marcelo/netflixclone/DetalhesFilme.kt
+++ app/src/main/java/com/marcelo/netflixclone/DetalhesFilme.kt
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.GridLayoutManager
 import com.marcelo.netflixclone.Adapter.FilmesAdapter
 import com.marcelo.netflixclone.Model.addFilmes
 import com.marcelo.netflixclone.databinding.ActivityDetalhesFilmeBinding
+import com.squareup.picasso.Picasso
 
 class DetalhesFilme : AppCompatActivity() {
 
@@ -23,6 +24,9 @@ class DetalhesFilme : AppCompatActivity() {
         val recycler_outros_filmes = binding.recyclerOutrosFilmes
         recycler_outros_filmes.adapter = FilmesAdapter(addFilmes())
         recycler_outros_filmes.layoutManager = GridLayoutManager(applicationContext, 3)
+
+        val capaTheWitcher = "https://firebasestorage.googleapis.com/v0/b/netflix-clone-c5ff9.appspot.com/o/video.jpg?alt=media&token=940da3e1-2b94-4729-a402-071f5c01f0eb"
+        Picasso.get().load(capaTheWitcher).fit().into(binding.capaDetalhe)
     }
 
     private  fun Toolbar() {
